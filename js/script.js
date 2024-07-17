eb636e1586a78ebbce4b020bfa406b927ebc38d8
vellandth <vellanindhita.npr>
2024-07-17 19:23:04 +0700

    progres: menyelesaikan data output

--- js/script.js
+++ js/script.js
@@ -50,23 +50,38 @@ function calculateBMI() {
     // Getting input for age
     let age = parseInt(document.querySelector("#input-usia").value);
 
-    let result = document.querySelector("#result");
+
+    //result
+    let outputStatus = document.querySelector("#output-status");
+    let outputNumber = document.querySelector("#output-number");
+    let outputInfo = document.querySelector("#output-info");
 
     // Validasi usia minimal 20 tahun
     if (age < 20) {
-        result.innerHTML = "Usia minimal harus 20 tahun untuk menghitung BMI!";
+        outputStatus.innerHTML = "Usia minimal harus 20 tahun untuk menghitung BMI!";
+        outputNumber.innerHTML = "";
+        outputInfo.innerHTML = "";
     } else if (isNaN(height) || height <= 0 || isNaN(weight) || weight <= 0) {
-        result.innerHTML = "Provide valid Height and Weight!";
+        outputStatus.innerHTML = "Masukan nilai yang benar!";
+        outputNumber.innerHTML = "";
+        outputInfo.innerHTML = "";
     } else {
         // Calculate the BMI
         let bmi = (weight / (height * height)).toFixed(2);
 
         // Determine BMI category
-        if (bmi < 18.6)
-            result.innerHTML = `Underweight : <span>${bmi}</span>`;
-        else if (bmi >= 18.6 && bmi < 24.9)
-            result.innerHTML = `Normal weight : <span>${bmi}</span>`;
-        else
-            result.innerHTML = `Overweight : <span>${bmi}</span>`;
+        if (bmi < 18.6) {
+            outputStatus.innerHTML = "Underweight";
+            outputNumber.innerHTML = `<span>${bmi}</span>`;
+            outputInfo.innerHTML = "Untuk mencapai berat badan yang sehat, pilih makanan tinggi kalori seperti alpukat, kacang-kacangan, dan daging tanpa lemak. Perbanyak asupan protein dengan telur, ikan, dan produk susu rendah lemak. Latihan resistensi seperti angkat beban akan membantu membangun massa otot.";
+        } else if (bmi >= 18.6 && bmi < 24.9) {
+            outputStatus.innerHTML = "Normal weight";
+            outputNumber.innerHTML = `<span>${bmi}</span>`;
+            outputInfo.innerHTML = "Pertahankan berat badan ideal dengan makan seimbang, termasuk serat dari buah-buahan dan sayuran, protein dari daging tanpa lemak atau sumber nabati, serta lemak sehat dari alpukat dan kacang-kacangan. Lakukan olahraga aerobik seperti berlari atau berenang minimal 30 menit setiap hari untuk kesehatan jantung yang baik.";
+        } else {
+            outputStatus.innerHTML = "Overweight";
+            outputNumber.innerHTML = `<span>${bmi}</span>`;
+            outputInfo.innerHTML = "Kurangi asupan kalori dengan menghindari makanan olahan dan gula tambahan. Fokus pada sayuran berwarna-warni, biji-bijian utuh, dan protein rendah lemak. Lakukan kombinasi latihan kardio seperti bersepeda atau berjalan cepat dengan latihan kekuatan seperti yoga atau pilates untuk membakar lemak dan memperkuat otot.";
+        }
     }
 }
